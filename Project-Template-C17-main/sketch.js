bf58cd91fdb2c8cbcb5afcce02dd700b1963783c
Jonathan20083 <103178313+Jonathan20083@users.noreply.github.com>
2022-05-23 18:33:34 +0930

    Add files via upload

--- Project-Template-C17-main/sketch.js
+++ Project-Template-C17-main/sketch.js
@@ -49,10 +49,9 @@ mainCyclist.scale=0.07;
   
 //set collider for mainCyclist
 
-mainCyclist.setCollission("rectangle",0,0,40,40);
+
 mainCyclist.setCollider("rectangle",0,0,40,40);
-mainCyclist.setCollission("rectangle",0,0,40,40,50);
-mainCyclist.setCollider("rectangle",0,0,40,40,50);
+
 
   
 gameOver = createSprite(650,150);
@@ -145,17 +144,7 @@ function draw() {
     redCG.setVelocityXEach(0);
     redCG.setLifetimeEach(-1);
     
-     if(keyDown("UP_ARROW")) {
-       reset;
-     }
-
-     if(key("UP_ARROW")) {
-       reset();
-     }
-
-     if(keyDown()) {
-       reset();
-     }
+     
 
      if(keyDown("UP_ARROW")) {
        reset();
@@ -190,29 +179,9 @@ function redCyclists(){
         redCG.add(player3);
 }
 
-function reset{
-  gameState = END;
-  gameOver.visible = false;
-  mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
-  
-  pinkCG.destroyEach();
-  yellowCG.destroyEach();
-  redCG.destroyEach();
-  
-  distance = 0;
- }
 
-function reset {
-  gameState = PLAY;
-  gameOver.visible = true;
-  mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
-  
-  pinkCG.destroy();
-  yellowCG.destroy();
-  redCG.destroy();
-  
-  distance = 0;
- }
+
+
 
 function reset(){
   gameState = PLAY;
@@ -226,15 +195,7 @@ function reset(){
   distance = 0;
  }
 
-function reset(){
-  gameState = END;
-  gameOver.visible = true;
-  mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
-  
-  pinkCG.destroyEach();
-  redCG.destroyEach();
-  
-  distance = 50;
- }
+
+ 
 
 
